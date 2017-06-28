664d954bb7a628e0da715c558972765e2769ca19
nidhisingh123 <singhn4445@gmail.com>
2017-06-28 00:08:18 -0500

    new user component route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LogoutComponent } from './account/logout.component';
 import { UserDetailComponent } from './user/detail.component';
+import {NewUserComponent} from "./new-user/new-user.component";
+
 
 const mainRoutes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'logout', component: LogoutComponent },
+  { path: 'users/create', component: NewUserComponent },
   { path: 'users/:id', component: UserDetailComponent }
 ];
 
